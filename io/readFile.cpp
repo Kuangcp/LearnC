013b5f0222e9e96419c59271769200c6d1ac1986
kuangcp <kuangcp@aliyun.com>
2018-03-27 17:48:18 +0800

    add comment

--- io/readFile.cpp
+++ io/readFile.cpp
@@ -1,13 +1,13 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h> // 文件操作需要引入的头文件
 int main(){
-	FILE *fp;
+	FILE *fp; // 定义文件指针, 
 	char ch;
-	if ((fp=fopen("data.ini","r"))==NULL){
+	if ((fp=fopen("data.ini","r"))==NULL){// 以只读的方式打开文件, 如果文件不存在就提示并退出
 		printf ("this file cannot opened\n");
 		exit(1);
 	}
-	while ((ch=fgetc(fp))!=EOF){
+	while ((ch=fgetc(fp))!=EOF){ // 循环输出文件内容, 直到读到了文件的结束符,
 		fputc(ch,stdout);
 	}
 	printf("\n");
--- io/writeFile.cpp
+++ io/writeFile.cpp
@@ -1,18 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
-{
+int main(){
 	FILE *fp1;
 	char ch;
 	if ((fp1=fopen("data.ini","a"))==NULL){// 从键盘输入 向文件写入字符以 # 结束运行
 		printf ("cannot open file\n");
 		exit (0);
 	}
-	ch=getchar();
-	while (ch!='#'){
-		fputc(ch,fp1);
-		ch=getchar();
+	ch=getchar(); // 获取键盘输入的函数
+	while (ch!='#'){ // 循环读取输入, 写入文件 直到读取到 # 
+		fputc(ch,fp1); // 将上面获取的字符 输出到文件中
+		ch=getchar(); // 读取键盘输入
 	}
-	fclose(fp1);
+	fclose(fp1); // 关闭文件
 	return 0;
 }
