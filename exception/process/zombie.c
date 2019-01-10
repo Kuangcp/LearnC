547b375e16882eb6bdaabd4a6f2cafbb96c715a0
kuangcp <kuangcp@aliyun.com>
2019-01-10 14:57:45 +0800

    More accurate description

--- exception/process/zombie.c
+++ exception/process/zombie.c
@@ -16,10 +16,10 @@ int main()
     {
         printf("I am child process.I am exiting.\n");
         exit(0);
-        // 至此, 该子进程就是 僵死进程
+        // 子进程已结束,此后 该子进程就是 僵死进程
     }
     printf("I am father process.I will sleep two seconds\n");
-    //等待子进程先退出
+    //等待子进程先结束
     sleep(2);
     
     //输出进程信息
@@ -27,5 +27,12 @@ int main()
     printf("father process is exiting.\n");
     
     return 0;
-    // 至此, 父进程就执行结束了, 所以父进程会退出, 子进程也会被回收
+    // 至此, 父进程就执行结束了
+    // 当父进程结束后,原来的僵死子进程, 会成为孤儿进程且是僵死进程, 此时会被1号进程收养, 最终也会被回收  
+    
+    // https://yq.aliyun.com/articles/61894
+    // 在 Docker 中, 由于没有 init 这个1号进程(往往是应用进程作为1号进程) 很有可能子进程称为僵死进程且一直存在  
+    // Docker 1.11 之前的版本，孤儿进程是否有可能成为僵尸进程取决于容器内pid为1的进程是否在子进程退出时调用wait/waitpid
+    // Docker 1.11 版本之后孤儿进程不会成为僵尸进程
+    //
 }
