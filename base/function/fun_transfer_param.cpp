739a952006a5bf5a2a442e5df63d5afbcb716f7d
Kuangcp <kuangcp@aliyun.com>
2020-01-30 17:40:20 +0800

    *) simplify

--- base/function/fun_transfer_param.cpp
+++ base/function/fun_transfer_param.cpp
@@ -1,55 +1,55 @@
 #include "stdio.h"
+#include "stdlib.h"
 
-void showInfo(char *str, int *a, int *b)
+#define NUM 9
+
+// 为什么 Java 只有值传递，但 C# 既有值传递，又有引用传递，这种语言设计有哪些好处？ - 知乎用户的回答 - 知乎
+// https://www.zhihu.com/question/20628016/answer/28970414
+
+void showInfo(char *str, int *a)
 {
-    printf("%s \n a = %d, &a = %p \n b = %d, &b = %p\n", str, *a, a, *b, b);
+    printf("%s \n a = %d, &a = %p \n", str, *a, a);
 }
 
 // 值传递，形参 a b 都是新内存地址
-void swap(int a, int b)
+void assign(int a)
 {
-    showInfo("swap before", &a, &b);
-    int tmp = a;
-    a = b;
-    b = tmp;
-    showInfo("swap after", &a, &b);
+    a = NUM;
+    showInfo("assign after", &a);
 }
 
 // 值传递(传递地址值)，形参 a b 都是指向实参内存地址
 // 能改变实参的值但是无法改变实参指针指向
-void swapWithPoint(int *a, int *b)
+void assignWithPoint(int *a)
 {
-    int tmp = *b;
-    *b = *a;
-    *a = tmp;
-    showInfo("swapWithPoint after", a, b);
+    *a = NUM;
+    showInfo("assignWithPoint after", a);
 }
 
-// C++ 语法 引用传递 此时实参和形参可视为等价
-void swapWithQuote(int &a, int &b)
+// C++ 语法 引用传递 此时实参和形参可视为等价 但是也无法改变实参指针指向
+void assignWithQuote(int &a)
 {
-    int tmp = a;
-    a = b;
-    b = tmp;
-    showInfo("swapWithQuote after", &a, &b);
+    a = NUM;
+    showInfo("assignWithQuote after", &a);
 }
 
 int main()
 {
     int a = 1;
-    int b = 2;
-    showInfo("main before", &a, &b);
-    swap(a, b);
-    showInfo("main after", &a, &b);
+    showInfo("main before", &a);
+    assign(a);
+    showInfo("main after", &a);
 
+    a = 1;
     printf("\n\n");
-    showInfo("main before", &a, &b);
-    swapWithPoint(&a, &b);
-    showInfo("main after", &a, &b);
+    showInfo("main before", &a);
+    assignWithPoint(&a);
+    showInfo("main after", &a);
 
+    a = 1;
     printf("\n\n");
-    showInfo("main before", &a, &b);
-    swapWithQuote(a, b);
-    showInfo("main after", &a, &b);
+    showInfo("main before", &a);
+    assignWithQuote(a);
+    showInfo("main after", &a);
     return 0;
 }
